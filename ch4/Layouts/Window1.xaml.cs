7a3ffd13be8ff2878fe31382a5ea350ab145e8bd
ngbrown <nathan@nkbrown.us>
2010-07-10 18:43:37 -0500

    Listing 4.6

--- ch4/Layouts/Window1.xaml.cs
+++ ch4/Layouts/Window1.xaml.cs
@@ -19,14 +19,27 @@ namespace Layouts
     /// </summary>
     public partial class Window1 : Window
     {
-        public Window1()
-        {
-            InitializeComponent();
-        }
+        Button button1 = null;
+        Button button2 = null;
+        Button button3 = null;
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            button1 = new Button { Content = "Button", Width = 70, Height = 23 };
+            Canvas.SetLeft(button1, 119);
+            Canvas.SetTop(button1, 24);
+            canvas1.Children.Add(button1);
 
+            button2 = new Button { Content = "Button2 is quite wide" };
+            Canvas.SetLeft(button2, 44);
+            Canvas.SetTop(button2, 69);
+            canvas1.Children.Add(button2);
+
+            button3 = new Button { Content = "Button" };
+            Canvas.SetLeft(button3, 78);
+            Canvas.SetTop(button3, 119);
+            button3.Padding = new Thickness(10, 2, 10, 2);
+            canvas1.Children.Add(button3);
         }
     }
 }
