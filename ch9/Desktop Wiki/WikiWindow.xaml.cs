ccee5ca8d7114d2008e69c4b2bf37d23bd4e10e1
ngbrown <nathan@nkbrown.us>
2010-07-23 22:43:14 -0500

    Listing 9.1

--- ch9/Desktop Wiki/WikiWindow.xaml.cs
+++ ch9/Desktop Wiki/WikiWindow.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Desktop_Wiki
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for WikiWindow.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class WikiWindow : Window
     {
-        public Window1()
+        public WikiWindow()
         {
             InitializeComponent();
         }
