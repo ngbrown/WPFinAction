5b99671d893f39392be41f2308b64eb6962852f3
ngbrown <nathan@nkbrown.us>
2010-08-01 13:57:01 -0500

    Copied ProcessMonitor from ch 11.

---
+++ ch12/ProcessMonitor/Monitor.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ProcessMonitor
+{
+    /// <summary>
+    /// Interaction logic for Monitor.xaml
+    /// </summary>
+    public partial class Monitor : Window
+    {
+        public Monitor()
+        {
+            InitializeComponent();
+            BindProcessesToListView();
+        }
+
+        private void BindProcessesToListView()
+        {
+            ObjectDataProvider provider = new ObjectDataProvider();
+            provider.ObjectType = typeof(Process);
+            provider.MethodName = "GetProcesses";
+
+            Binding binding = new Binding();
+            binding.Source = provider;
+            binding.Mode = BindingMode.OneWay;
+
+            PresentationTraceSources.SetTraceLevel(binding, PresentationTraceLevel.High);
+
+            listView1.SetBinding(ListView.ItemsSourceProperty, binding);
+        }
+    }
+}
