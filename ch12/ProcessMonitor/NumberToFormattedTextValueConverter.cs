8e19a263c7519f4a1e2b00720b04d1a890dd54ce
ngbrown <nathan@nkbrown.us>
2010-08-01 14:38:55 -0500

    Listing 12.3

--- ch12/ProcessMonitor/NumberToFormattedTextValueConverter.cs
+++ ch12/ProcessMonitor/NumberToFormattedTextValueConverter.cs
@@ -11,12 +11,36 @@ namespace ProcessMonitor
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             Int64 size = System.Convert.ToInt64(value);
-            size = size / 1024;
+            string units = (parameter != null) ? parameter.ToString() : "IEC";
 
-            if (size < 1024)
-                return size.ToString() + " KiB";
-            else
-                return (size / 1024).ToString() + " MiB";
+            switch(units)
+            {
+                case "IEC":
+                    size = size / 1024;
+
+                    if (size < 1024)
+                        return size.ToString() + " KiB";
+                    else
+                        return (size / 1024).ToString() + " MiB";
+
+                case "BINARYSI":
+                    size = size / 1024;
+
+                    if (size < 1024)
+                        return size.ToString() + " KB";
+                    else
+                        return (size / 1024).ToString() + " MB";
+
+                case "SI":
+                    size = size / 1000;
+
+                    if (size < 1000)
+                        return size.ToString() + " KB";
+                    else
+                        return (size / 1024).ToString() + " MB";
+            }
+
+            return "Bad Param";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
