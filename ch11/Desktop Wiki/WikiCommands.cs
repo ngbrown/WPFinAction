d61fefe62a83a0bcf46c740d4a9a746603160280
ngbrown <nathan@nkbrown.us>
2010-08-01 00:40:39 -0500

    Copied Wiki application form chapter 10

---
+++ ch11/Desktop Wiki/WikiCommands.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+namespace Desktop_Wiki
+{
+    public static class WikiCommands
+    {
+        private static RoutedUICommand createLinkFromSelection;
+
+        static WikiCommands()
+        {
+            createLinkFromSelection = new RoutedUICommand(
+                "Create Link from Selection",
+                "CreateLinkFromSelection",
+                typeof(WikiCommands));
+        }
+
+        public static RoutedUICommand CreateLinkFromSelection
+        {
+            get { return createLinkFromSelection; }
+        }
+    }
+}
---
+++ ch11/Desktop Wiki/WikiWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop_Wiki
+{
+    /// <summary>
+    /// Interaction logic for WikiWindow.xaml
+    /// </summary>
+    public partial class WikiWindow : Window
+    {
+        public static RoutedCommand About = new RoutedCommand();
+
+        static WikiWindow()
+        {
+            About.InputGestures.Add(new KeyGesture(Key.F3));
+            About.InputGestures.Add(new MouseGesture(MouseAction.RightDoubleClick, ModifierKeys.Control));
+        }
+
+        public WikiWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutExecuted(object sender, ExecutedRoutedEventArgs args)
+        {
+            MessageBox.Show("Welcome to WikiInAction");
+        }
+
+        private void CreateLinkFromSelectionExecuted(object sender, ExecutedRoutedEventArgs args)
+        {
+
+        }
+
+        private void CreateLinkFromSelectionCanExecute(object sender, CanExecuteRoutedEventArgs args)
+        {
+            RichTextBox wikiEditor = sender as RichTextBox;
+            args.CanExecute = !wikiEditor.Selection.IsEmpty;
+            args.ContinueRouting = false;
+            args.Handled = true;
+        }
+    }
+}
