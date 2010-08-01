72f8009d2ac2c08c1655d8f175e0e1b5bb21a144
ngbrown <nathan@nkbrown.us>
2010-08-01 00:55:16 -0500

    Listing 11.12

---
+++ ch11/Desktop Wiki/PageDirectory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Desktop_Wiki
+{
+    [Serializable]
+    public class PageDirectory
+    {
+        public PageDirectory() { }
+        public PageDirectory(string name)
+        {
+            name = name;
+            pages = new ObservableCollection<WikiPage>();
+        }
+
+        private string collectionName;
+        public string Name
+        {
+            get { return collectionName; }
+            set { collectionName = value; }
+        }
+
+        private ObservableCollection<WikiPage> pages;
+        public ObservableCollection<WikiPage> Pages
+        {
+            get { return pages; }
+            set { pages = value; }
+        }
+    }
+}
