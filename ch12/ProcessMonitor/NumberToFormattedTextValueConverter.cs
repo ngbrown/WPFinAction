1efd10d538eca9053288eb662ed994e4941e5b8d
ngbrown <nathan@nkbrown.us>
2010-08-01 14:15:03 -0500

    Listing 12.2

---
+++ ch12/ProcessMonitor/NumberToFormattedTextValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace ProcessMonitor
+{
+    public class NumberToFormattedTextValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Int64 size = System.Convert.ToInt64(value);
+            size = size / 1024;
+
+            if (size < 1024)
+                return size.ToString() + " KiB";
+            else
+                return (size / 1024).ToString() + " MiB";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
