a3b2fb237184d594d3082a0ad203b008bfa5f5e3
ngbrown <nathan@nkbrown.us>
2010-08-01 00:31:52 -0500

    Listin 11.9

--- ch11/Librarian/BookmarkLibrary.xaml.cs
+++ ch11/Librarian/BookmarkLibrary.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Librarian
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for BookmarkLibrary.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class BookmarkLibrary : Window
     {
-        public Window1()
+        public BookmarkLibrary()
         {
             InitializeComponent();
         }
