9ee9f32322bf848b97de495263a269430165cfb3
ngbrown <nathan@nkbrown.us>
2010-08-07 17:29:49 -0500

    Listing 12.13

---
+++ ch12/Librarian/UriRule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Librarian
+{
+    public class UriRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            bool uriIsValid = false;
+            string message;
+
+            try
+            {
+                Uri uri = new Uri((string)value, UriKind.Absolute);
+                uriIsValid = true;
+                message = "URI is valid";
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+
+            return new ValidationResult(uriIsValid, message);
+        }
+    }
+}
