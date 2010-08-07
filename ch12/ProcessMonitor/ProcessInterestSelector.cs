418c73701b02cd1b75c5cc291665e2a63582df8a
ngbrown <nathan@nkbrown.us>
2010-08-07 16:04:25 -0500

    Listing 12.11

---
+++ ch12/ProcessMonitor/ProcessInterestSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Diagnostics;
+
+namespace ProcessMonitor
+{
+    public class ProcessInterestSelector : DataTemplateSelector
+    {
+        public Int64 Threshold { get; set; }
+        public DataTemplate NormalTemplate { get; set; }
+        public DataTemplate InterestingTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            Process process = (Process)item;
+
+            if (process.WorkingSet64 > Threshold)
+                return InterestingTemplate;
+
+            return NormalTemplate;
+        }
+    }
+}
