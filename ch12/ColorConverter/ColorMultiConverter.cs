8ec24428a5bf6f352a0b8f6d47cfa878804196be
ngbrown <nathan@nkbrown.us>
2010-08-07 21:07:48 -0500

    Listing 12.14

---
+++ ch12/ColorConverter/ColorMultiConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorConverter
+{
+    class ColorMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            byte R = System.Convert.ToByte((double)values[0]);
+            byte G = System.Convert.ToByte((double)values[1]);
+            byte B = System.Convert.ToByte((double)values[2]);
+
+            Color newColor = Color.FromRgb(R, G, B);
+
+            return newColor;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
