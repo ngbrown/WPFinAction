86a93517bf9f6dc5c210fc730627f4ed437b4e4d
ngbrown <nathan@nkbrown.us>
2010-08-08 12:35:40 -0500

    Listing 13.1, listing 13.2, and figure 13.2

--- ch13/ControlsInAction/LinkLabel.xaml.cs
+++ ch13/ControlsInAction/LinkLabel.xaml.cs
@@ -23,5 +23,55 @@ namespace ControlsInAction
         {
             InitializeComponent();
         }
+
+        public static readonly DependencyProperty TextProperty = 
+            DependencyProperty.Register(
+            "Text", typeof(string), typeof(LinkLabel), 
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnTextChanged)));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty UriProperty =
+            DependencyProperty.Register(
+            "Uri", typeof(string), typeof(LinkLabel),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnUriChanged)));
+
+        public string Uri
+        {
+            get { return (string)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
+        }
+
+        private static void OnTextChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            LinkLabel label = (LinkLabel)sender;
+            label.webLink.Inlines.Clear();
+            label.webLink.Inlines.Add(new Run(e.NewValue.ToString()));
+        }
+
+        private static void OnUriChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            LinkLabel label = (LinkLabel)sender;
+            Uri newUri;
+            try
+            {
+                newUri = new Uri(label.Uri);
+                label.webLink.NavigateUri = newUri;
+                label.webLink.ToolTip = String.Format("Open a new browser to {0}", label.Uri);
+            }
+            catch (UriFormatException ex)
+            {
+                label.webLink.ToolTip = String.Format("{0} ({1})", ex.Message, label.Uri);
+            }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.ToString());
+        }
     }
 }
