d59ded207d5c10d8d5cee118dbf88b2338a7b1ea
ngbrown <nathan@nkbrown.us>
2010-08-13 22:27:45 -0500

    section 20.1.2

--- ch19/WpfWithWinForms/App.xaml.cs
+++ ch19/WpfWithWinForms/App.xaml.cs
@@ -12,5 +12,9 @@ namespace WpfWithWinForms
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+        }
     }
 }
