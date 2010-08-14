354a9b30964750b2d03234831ea17a80c3d004c5
ngbrown <nathan@nkbrown.us>
2010-08-14 00:33:04 -0500

    Figure 20.16

--- ch20/WinFormsWithWpf/WinFormsWithWpf/Form1.Designer.cs
+++ ch20/WinFormsWithWpf/WinFormsWithWpf/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.calculatorControl1 = new MyWPFControlLibrary.CalculatorControl();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(284, 262);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.calculatorControl1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private MyWPFControlLibrary.CalculatorControl calculatorControl1;
     }
 }
 
